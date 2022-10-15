e3f7b39fbedfbe2a3b4b54c75e3f6ab52ac40fd1
Briggs-Richardson <briggsbdr@gmail.com>
2022-10-15 14:12:52 -0700

    Development Commit
    This change creates a development branch for implementing the "Load
    Video File" functionality. This initial commit creates a menu bar at the
    top of the application.

--- View/main_window.py
+++ View/main_window.py
@@ -7,9 +7,19 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         """
-        Constructor - Initializes the properties of the main window.
+        Constructor - Initializes the properties of the main window and all
+        containing GUI elements.
         """
         super().__init__()
 
         self.setWindowTitle("Qualitative Coding Desktop App")
         self.setWindowState(Qt.WindowMaximized)
+
+        self.create_menu_bar()
+
+    def create_menu_bar(self):
+        """
+        Creates the main menu-bar for the application window and populates it with a
+        File sub-menu.
+        """
+        file_menu = self.menuBar().addMenu("File")
